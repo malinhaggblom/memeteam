7d473d14733dccb23974bfb40c86920f84185ea9
HeikkiHei <heikkihei.ahonen@gmail.com>
2021-12-07 14:20:20 +0200

    simple SQL base

--- src/AgileGroupwork/Program.cs
+++ src/AgileGroupwork/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Data.SQLite;
 
 namespace Groupwork
 {
@@ -6,7 +8,47 @@ namespace Groupwork
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // At the moment, destroy the database and begin anew
+            if (File.Exists("MyDatabase.sqlite"))
+            {
+                File.Delete("MyDatabase.sqlite");
+            }
+            SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            Console.WriteLine("Created a database!");
+
+            // Open a database connection
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            m_dbConnection.Open();
+
+            // Create a table
+            string sql = "CREATE TABLE Highscores (name TEXT, score INTEGER)";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+            // Insert dummy data
+            sql = "INSERT INTO Highscores (name, score) VALUES ('Me', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "INSERT INTO Highscores (name, score) VALUES ('Myself', 6000)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+            sql = "INSERT INTO Highscores (name, score) VALUES ('And I', 9001)";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            command.ExecuteNonQuery();
+
+
+
+            // Read the data from the database
+            sql = "SELECT * FROM Highscores ORDER BY score DESC";
+            command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("Name: " + reader["name"] + "\tScore: " + reader["score"]);
+            }
+
+            // Close the connection
+            m_dbConnection.Close();
         }
     }
 }
