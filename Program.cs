60b2e6f2a0f48400a819ed1cf821328d7ebebaf8
HeikkiHei <heikkihei.ahonen@gmail.com>
2021-12-07 13:48:04 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace agile_groupwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
