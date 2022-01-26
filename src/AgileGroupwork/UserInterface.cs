07ace86d8f0b180f3be57c003271f302c079e2ab
Malin Haggblom <malin.haggblom@example.com>
2022-01-26 12:28:49 +0200

    Added functioning code

---
+++ src/AgileGroupwork/UserInterface.cs
@@ -0,0 +1,50 @@
+namespace MemeTeamPro
+{
+    using System;
+    using System.Collections.Generic;
+    public class UserInterface
+    {
+        private List<Score> list;
+        public UserInterface()
+        {
+            this.list = new List<Score>();
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter command");
+                Console.WriteLine("1 - Add a score for a person");
+                Console.WriteLine("2 - Show a person's score");
+                Console.WriteLine("3 - Show all scores");
+                Console.WriteLine("x - Quit");
+                string command = Console.ReadLine();
+                if (command == "x" || command == "X")
+                {
+                    break;
+                }
+                if (command == "1")
+                {
+                    Console.WriteLine("Person's name");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Score");
+                    int score = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Type of score");
+                    string type = Console.ReadLine();
+                    list.Add(new Score(name, score, type));
+                    Console.WriteLine("Thank you! Added " + name + "'s " + score + " scores to the scorelist!");
+                }
+                if (command == "2")
+                {
+                    Console.WriteLine("Which person's score would you like to see");
+                    string person = Console.ReadLine();
+                    Console.WriteLine("Here are the scores for " + person);
+                }
+                if (command == "3")
+                {
+                    Console.WriteLine("Here are all the scores");
+                }
+            }
+        }
+    }
+}
