a3edc68f86013be1b5a55880c42aaf4c6b73b9a8
Malin Haggblom <malin.haggblom@example.com>
2022-01-28 10:42:04 +0200

    Changed the text

--- src/AgileGroupwork/UserInterface.cs
+++ src/AgileGroupwork/UserInterface.cs
@@ -4,23 +4,28 @@ namespace MemeTeamPro
     using System.Collections.Generic;
     public class UserInterface
     {
-        private List<Score> list;
         public UserInterface()
-        {
-            this.list = new List<Score>();
-        }
+        {}
         public void Start()
         {
+            Console.WriteLine();
+            Console.WriteLine("Enter username");
+            string username = Console.ReadLine();
+            Console.WriteLine("Enter password");
+            string password = Console.ReadLine();
             while (true)
             {
+                Console.WriteLine();
                 Console.WriteLine("Enter command");
                 Console.WriteLine("1 - Add a score for a person");
                 Console.WriteLine("2 - Show a person's score");
                 Console.WriteLine("3 - Show all scores");
                 Console.WriteLine("x - Quit");
                 string command = Console.ReadLine();
+                Console.WriteLine();
                 if (command == "x" || command == "X")
                 {
+                    Console.WriteLine("Thank you! Bye!");
                     break;
                 }
                 if (command == "1")
@@ -31,18 +36,17 @@ namespace MemeTeamPro
                     int score = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine("Type of score");
                     string type = Console.ReadLine();
-                    list.Add(new Score(name, score, type));
-                    Console.WriteLine("Thank you! Added " + name + "'s " + score + " scores to the scorelist!");
+                    Console.WriteLine("Thank you! Added " + name + "'s " + score + " scores to the " + type + " scorelist!");
                 }
                 if (command == "2")
                 {
                     Console.WriteLine("Which person's score would you like to see");
                     string person = Console.ReadLine();
-                    Console.WriteLine("Here are the scores for " + person);
+                    Console.WriteLine("Here are all the scores for " + person + ":");
                 }
                 if (command == "3")
                 {
-                    Console.WriteLine("Here are all the scores");
+                    Console.WriteLine("Here are all the scores:");
                 }
             }
         }
